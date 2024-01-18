4c619b53a478e4a9600cb4de995d4dabfcb506e4
Meriem Chikha <meriem.chikha@laposte.net>
2024-01-18 23:38:59 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// importer le module dotenv pour accéder aux variables d'environnement
+require('dotenv').config();
+
+//définir les variables 
+const name = process.env.MY_NAME;
+const city = process.env.MY_CITY;
+const language = process.env.MY_LANGUAGE;
+
+//afficher le message sur le console
+console.log(`I am ${name}, wilder in ${city}, and I love ${language}.`);
